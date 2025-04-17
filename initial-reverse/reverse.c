fd4a321eaf0cf012291e1f3e2b413cf825780ad7
Cole Davis <dylcdav@gmail.com>
2025-04-17 19:53:19 -0400

    Add stderr outputs

--- initial-reverse/reverse.c
+++ initial-reverse/reverse.c
@@ -6,7 +6,11 @@ int print_and_return(FILE* fin, FILE* out);
 
 int main(int argc, char* argv[argc+1]) 
 {
-    // TODO: add argc > 3 check
+    if (argc > 3) {
+        printf("usage: reverse <input> <output>");
+        fprintf(stderr, "usage: reverse <input> <output>");
+        return 1;
+    }
 
     if (argc == 1) {
         print_and_return(stdin, stdout);
@@ -17,6 +21,8 @@ int main(int argc, char* argv[argc+1])
         FILE* fin = fopen(argv[1], "r");
         if (fin == NULL) {
             printf("error: cannot open file '%s'", argv[1]);
+            fprintf(stderr, "error: cannot open file '%s'", argv[1]);
+            
             return 1;
         }
 
@@ -29,18 +35,21 @@ int main(int argc, char* argv[argc+1])
         int v = strcmp(argv[1], argv[2]);
         if (v == 0) {
             printf("Input and output file must differ");
+            fprintf(stderr, "Input and output file must differ");
             return 1;
         };
 
         FILE* fin = fopen(argv[1], "r");
         if (fin == NULL) {
             printf("error: cannot open file '%s'", argv[1]);
+            fprintf(stderr, "error: cannot open file '%s'", argv[1]);
             return 1;
         }
 
         FILE* fout = fopen(argv[2], "w");
         if (fout == NULL) {
             printf("error: cannot open file '%s'", argv[2]);
+            fprintf(stderr, "error: cannot open file '%s'", argv[2]);
             return 1;
         }
 
